e28a0eb0d05ad410bc78aa6ab4328a1788d0ffd7
ElijahGut <s1740779@ed.ac.uk>
2024-02-07 08:58:11 +0000

    testing sus noam

--- noam.py
+++ noam.py
@@ -6,6 +6,8 @@ class Noam():
         self.d_model = d_model
         self.n_warmup_steps = n_warmup_steps
         self.n_steps = 0
+        self.force_decay = False
+        self.force_steps = 130000
 
 
     def step_and_update_lr(self):
@@ -22,6 +24,10 @@ class Noam():
     def _get_lr_scale(self):
         d_model = self.d_model
         n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
+        # print(f'step {n_steps}, lhs {n_steps ** (-0.5)}, rhs {n_steps * n_warmup_steps ** (-1.5)}')
+        # print(self.force_decay)
+        if self.force_decay:
+            return (d_model ** -0.5) * self.force_steps ** (-0.5)
         return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
 
 
@@ -29,7 +35,13 @@ class Noam():
         ''' Learning rate scheduling per step '''
 
         self.n_steps += 1
-        lr = self._get_lr_scale()
+        self.force_steps += 1
 
+        lr = self._get_lr_scale()
+        # print(lr)
+
+        if lr > 1e-04:
+            self.force_decay = True
+        
         for param_group in self._optimiser.param_groups:
             param_group['lr'] = lr
